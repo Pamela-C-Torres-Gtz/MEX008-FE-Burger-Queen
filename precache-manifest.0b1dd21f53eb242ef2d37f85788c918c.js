b175a7fa22244bfde28f8ad43dcd6fc9a58885ca
Pamela C. Torres Gutiérrez <pamelatgtz@gmail.com>
2019-10-03 00:05:39 -0500

    Updates

---
+++ precache-manifest.0b1dd21f53eb242ef2d37f85788c918c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa7dada1f2c5919ea273a2d2cfbd5df8",
+    "url": "/MEX008-FE-Burger-Queen/index.html"
+  },
+  {
+    "revision": "0ca57389996f16a3607e",
+    "url": "/MEX008-FE-Burger-Queen/static/css/2.4a1d8450.chunk.css"
+  },
+  {
+    "revision": "d9b19c538a5cb73fbcc1",
+    "url": "/MEX008-FE-Burger-Queen/static/css/main.818a342e.chunk.css"
+  },
+  {
+    "revision": "0ca57389996f16a3607e",
+    "url": "/MEX008-FE-Burger-Queen/static/js/2.1e20f6e8.chunk.js"
+  },
+  {
+    "revision": "d9b19c538a5cb73fbcc1",
+    "url": "/MEX008-FE-Burger-Queen/static/js/main.dcd7158b.chunk.js"
+  },
+  {
+    "revision": "1df6f6f19d1cb1f8397d",
+    "url": "/MEX008-FE-Burger-Queen/static/js/runtime~main.12b5020f.js"
+  },
+  {
+    "revision": "3997785d568253ee608815aa1bf747f3",
+    "url": "/MEX008-FE-Burger-Queen/static/media/hot-dog.3997785d.png"
+  },
+  {
+    "revision": "25b4db2a32747c4eb716c9126e50369b",
+    "url": "/MEX008-FE-Burger-Queen/static/media/lemonade.25b4db2a.png"
+  },
+  {
+    "revision": "0c3813aaf78aa430f8d20db0ae3b8d74",
+    "url": "/MEX008-FE-Burger-Queen/static/media/logodemonium.0c3813aa.png"
+  },
+  {
+    "revision": "5906a54d5e32cb36405255f4f7576a04",
+    "url": "/MEX008-FE-Burger-Queen/static/media/pizzaicon.5906a54d.png"
+  },
+  {
+    "revision": "aa5b3e0301b785645472fc55c7b8774b",
+    "url": "/MEX008-FE-Burger-Queen/static/media/salad.aa5b3e03.png"
+  }
+]);
